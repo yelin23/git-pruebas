5cb1534ccce996859194d475f003b3aeacdff77e
Deangerlin Loaiza <yelin_11@hotmail.com>
2018-10-15 10:47:52 -0430

    Se agrego el punto 8

---
+++ git_comandos.php
@@ -0,0 +1,34 @@
+1. Comando para cambiar nombre:
+    git config --global user.name "nombre"
+
+2. Comando para cambiar correo:
+    git config --global user.emal "correo"
+
+3. Comando para colocar los colores:
+    git config --global color.ul true
+
+4. Inicializar un repositorio
+    git init
+
+5. Visalizar estados de los archivos
+    git status
+
+6. Comando para indicar que archivos estan listos
+    git add (para agregar 1)
+    git -A (para agregar todos los archivos)
+
+7. Comando para guardar cambios en los archivos
+    git comit -m "mensaje para identificar el cambio realizado"
+
+8. Comando para visualizar los comits realizados
+    git log
+* Comandos de consola
+
+1. Moverse entre carpetas
+    cd /path(ruta)
+
+2. Listar archivos de un directorio
+    ls
+
+3. Borrar archivos
+    rm 
